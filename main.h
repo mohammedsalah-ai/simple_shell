6d05a6a46e71f758e68b31b0c02aa659fef30d75
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-08-13 18:10:31 +0000

    added main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+char **fetch_command(char **lineptr, size_t *n, FILE *stream, const char *delim);
+
+
+#endif /*_MAIN_H_*/
